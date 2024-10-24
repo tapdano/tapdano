49b25949fffe60c163c86e5dee472f5f3b4a62f8
edvxndxvi <edvandavi1@hotmail.com>
2024-10-24 17:01:46 -0300

    add Open Source section (in progress)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import './home.css'
-import logo from '../../logo.svg'
+import logo from '../../assets/logo.svg'
 import Header from '../../components/Header/Header'
 
 function Home() {
@@ -15,6 +15,14 @@ function Home() {
         </div>
         <p className='text gradient-text'>Simplifying the integration between NFC and NFT technologies in the <span >Cardano Blockchain</span></p>
       </main>
+
+      <section className='openSource container'>
+        <div className="openSourceContent">
+          <h1 className='title gradient-title'>Open-source development tools</h1>
+          <p className='text'>We create tools that simplify the development of applications using NFC and NFT technologies such as NFC wallets and physical product authentication. TapDano is a project funded by the <span>Cardano Catalyst Fund 10.</span></p>
+          <a href="#" className='button'>Explore Use Cases</a>
+        </div>
+      </section>
     </>
   )
 }
