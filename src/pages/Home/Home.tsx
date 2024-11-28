bab84d683d9c9b36960a3526e8aa4e3853eb915e
Lucas Boemeke <lucas@blueshift.cc>
2024-11-28 17:50:26 -0300

    hide video controls

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -24,7 +24,7 @@ function Home() {
       </section>
 
       <main className='container hero-home'>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/GacSn2CCE80?si=9SwtdwrPi43IByP3" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/GacSn2CCE80?si=9SwtdwrPi43IByP3&amp;controls=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       </main>
 
       <section className='openSource container'>
